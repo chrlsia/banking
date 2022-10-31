c406b55e2341eaac1a309e29751aead7abebbf1f
Chris Siannas <xrsia@otenet.gr>
2022-10-31 14:16:23 +0200

    json tags to Customer struct

--- main.go
+++ main.go
@@ -6,28 +6,28 @@ import (
 	"net/http"
 )
 
-type Customer struct{
-	Name string
-	City string
-	Zipcode string
+type Customer struct {
+	Name    string	`json:"full_name"`
+	City    string	`json:"city"`
+	Zipcode string	`json:"zip_code"`
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/greet", greet)
 	http.HandleFunc("/customers", getAllCustomers)
 
-	http.ListenAndServe(":3000",nil)
+	http.ListenAndServe(":3000", nil)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w,"Hello World!")
+	fmt.Fprint(w, "Hello World!")
 }
 
-func getAllCustomers(w http.ResponseWriter, r *http.Request ){
-	customers:=[]Customer{
-		{Name:"Ashish",City: "New Delhi", Zipcode:"110075"},
-		{Name:"Rod",City: "New Delhi", Zipcode:"110075"},
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Ashish", City: "New Delhi", Zipcode: "110075"},
+		{Name: "Rod", City: "New Delhi", Zipcode: "110075"},
 	}
-	w.Header().Add("Content-Type","application/json")
+	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customers)
 }
