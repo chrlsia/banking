f352216e517d6c01da6ad7f498a391998249aa3b
Chris Siannas <xrsia@otenet.gr>
2022-10-31 17:57:09 +0200

    Introducing github.com/gorilla/mux

--- app/app.go
+++ app/app.go
@@ -3,13 +3,18 @@ package app
 import (
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func Start(){
 
-	mux:=http.NewServeMux()
+	// mux:=http.NewServeMux()
+	mux:=mux.NewRouter()
 
+	// define routes
 	mux.HandleFunc("/greet", greet)
 	mux.HandleFunc("/customers", getAllCustomers)
+
+	// start the server
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
