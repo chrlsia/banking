f79ce0bb1a34f5e5226ed18c9c3262ff0c42a975
Chris Siannas <xrsia@otenet.gr>
2022-10-31 11:54:59 +0200

    Create function greet

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main(){
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprint(w,"Hello World!")
-	})
+	http.HandleFunc("/greet", greet)
 
 	http.ListenAndServe(":3000",nil)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"Hello World!")
 }
