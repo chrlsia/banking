035df47a89662b8af0e79315c6a6c1e492c6f681
Chris Siannas <xrsia@otenet.gr>
2022-10-31 14:07:36 +0200

    json.Encoding

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 type Customer struct{
-	Name,City,Zipcode string
+	Name string
+	City string
+	Zipcode string
 }
+
 func main(){
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	http.ListenAndServe(":3000",nil)
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w,"Hello World!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request ){
+	customers:=[]Customer{
+		{Name:"Ashish",City: "New Delhi", Zipcode:"110075"},
+		{Name:"Rod",City: "New Delhi", Zipcode:"110075"},
+	}
+	json.NewEncoder(w).Encode(customers)
 }
